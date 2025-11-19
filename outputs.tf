77d551b4ebe8579a73ee798a19327d6820793d3f
Martelete <henriquemartelete@gmail.com>
2025-11-19 17:50:56 +0000

    lambda-proxy: Fixing outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "rest_api_id" {
   value = aws_api_gateway_rest_api.api.id
 }
 
-output "lambda_name" {
-  value = module.go_lambda.lambda_function_name
-}
+# output "lambda_name" {
+#   value = module.go_lambda.lambda_function_name
+# }
